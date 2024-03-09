b3cb0bb1cbba82e11b5b5d255613a692afac010b
Stangli Adadurov <easybbs@gmail.com>
2024-03-09 15:54:28 +0300

    оптимизировать переходы между страницами

--- fixture/application.py
+++ fixture/application.py
@@ -8,7 +8,8 @@ class Application:
 
     def __init__(self):
         self.wd = WebDriver()
-        self.wd.implicitly_wait(5)
+        # совсем без ожидания нельзя, тесты не проходят
+        self.wd.implicitly_wait(1)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
