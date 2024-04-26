fca59f233eeeac43ba93f169d8da7bf60b141366
Stangli Adadurov <easybbs@gmail.com>
2024-04-26 19:36:58 +0300

    BDD add, del group

---
+++ bdd/group_scenarios.py
@@ -0,0 +1,13 @@
+_author__ = 'Stangli Adadurov'
+from pytest_bdd import scenario
+from .group_steps import *
+
+
+@scenario('groups.feature', 'Add new group')
+def test_add_new_group():
+    pass
+
+
+@scenario('groups.feature', 'Delete a group')
+def test_delete_group():
+    pass
