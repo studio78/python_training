5e4b2bcdd2a22b377478ed1106dae3a5fd1ad683
Stangli Adadurov <easybbs@gmail.com>
2024-04-30 09:47:18 +0300

    allure

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 # import pytest
+import allure
+
 from model.group import Group
 # from data.groups import testdata
 # from data.add_group import constant as testdata
+import allure
 
 '''
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
@@ -35,7 +38,6 @@ def test_add_group(app, json_groups):
     new_groups = app.group.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-'''
 
 
 def test_add_group(app, db, json_groups):
@@ -45,3 +47,16 @@ def test_add_group(app, db, json_groups):
     new_groups = db.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+'''
+
+
+def test_add_group(app, db, json_groups):
+    group = json_groups
+    with allure.step('Given a group list'):
+        old_groups = db.get_group_list()
+    with allure.step('When I add a group %s to the list' % group):
+        app.group.create(group)
+    with allure.step('Then the new group list is equal to the old list with the added group'):
+        new_groups = db.get_group_list()
+        old_groups.append(group)
+        assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,5 +1,6 @@
 from model.group import Group
 import random
+import allure
 
 '''
 def test_delete_some_group(app):
@@ -12,9 +13,8 @@ def test_delete_some_group(app):
     new_groups = app.group.get_group_list()
     old_groups[index:index+1] = []
     assert old_groups == new_groups
-'''
 
-
+    
 def test_delete_some_group(app, db, check_ui):
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name="test"))
@@ -27,3 +27,21 @@ def test_delete_some_group(app, db, check_ui):
     assert old_groups == new_groups
     if check_ui:
         assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
+'''
+
+
+def test_delete_some_group(app, db, check_ui):
+    if len(db.get_group_list()) == 0:
+        app.group.create(Group(name="test"))
+    with allure.step('Given a random group from the list'):
+        old_groups = db.get_group_list()
+        group = random.choice(old_groups)
+    with allure.step('When I delete the group from the list'):
+        app.group.delete_group_by_id(group.id)
+    with allure.step('Then The new group list is equal to the old list without deleted group'):
+        new_groups = db.get_group_list()
+        assert len(old_groups) - 1 == app.group.count()
+        old_groups.remove(group)
+        assert old_groups == new_groups
+        if check_ui:
+            assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
